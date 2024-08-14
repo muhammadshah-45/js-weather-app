b5ff1ae27c656b7b2b4aad43d63b10ebb15e1087
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-08-14 23:34:59 +0500

    i setup the whole project

---
+++ main.js
@@ -0,0 +1,13 @@
+import '/style.css';
+import { getWeather } from "/weather.js";
+
+
+
+
+
+
+
+getWeather(10,10,Intl.DateTimeFormat().resolvedOptions().timeZone).then(
+    res => {
+    console.log(res.data);
+})
---
+++ weather.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+//https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime
+
+//https://api.open-meteo.com/v1/forecast?current=temperature_2m,apparent_temperature,precipitation,rain,showers,weather_code,wind_speed_10m&minutely_15=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m,temperature_80m,temperature_120m,temperature_180m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,daylight_duration,precipitation_sum,precipitation_hours,wind_speed_10m_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime
+//
+export function getWeather(lat, lon, timezone) {
+  return axios
+    .get(
+      "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
+      {
+        params: {
+          latitude: lat,
+          longitude: lon,
+          timezone,
+        },
+      }
+    )
+    .then(({ data }) => {
+      return {
+        current: parseCurrentWeather(data),
+        // daily: parseDailyWeather(data),
+        // hourly: parseHourlyWeather(data),
+      }
+    })
+}
+
+
+function parseCurrentWeather({ current_weather, daily }) {
+  const {
+    temperature: currentTemp,
+    windspeed: windSpeed,
+    weathercode: iconCode,
+  } = current_weather
+  const {
+    temperature_2m_max: [maxTemp],
+    temperature_2m_min: [minTemp],
+    apparent_temperature_max: [maxFeelsLike],
+    apparent_temperature_min: [minFeelsLike],
+    precipitation_sum: [precip],
+  } = daily
+
+  return {
+    currentTemp: Math.round(currentTemp),
+    highTemp: Math.round(maxTemp),
+    lowTemp: Math.round(minTemp),
+    highFeelsLike: Math.round(maxFeelsLike),
+    lowFeelsLike: Math.round(minFeelsLike),
+    windSpeed: Math.round(windSpeed),
+    precip: Math.round(precip * 100) / 100,
+    iconCode,
+  }
+}
