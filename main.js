7630baafe5ab424fe2eb5639b2761c4176297c74
muhammadshah-45 <mshahaurangzeb45@gmail.com>
2024-08-15 22:11:16 +0500

    I add functionality for current weather

--- main.js
+++ main.js
@@ -1,13 +1,49 @@
 import '/style.css';
 import { getWeather } from "/weather.js";
+import { ICON_MAP } from './iconMap';
 
 
 
 
 
 
-
-getWeather(10,10,Intl.DateTimeFormat().resolvedOptions().timeZone).then(
-    res => {
-    console.log(res.data);
+getWeather(10,10,Intl.DateTimeFormat().resolvedOptions().timeZone)
+.then(renderWeather)
+.catch(e=>{
+    console.error(e)
+    alert("Error getting weather")
 })
+
+
+function renderWeather({current,daily,hourly}){
+    renderCurrentWeather(current)
+    // renderDailyWeather(daily)
+    // renderHourlyWeather(hourly)
+    document.body.classList.remove("blured")
+}
+
+//create function helper 
+
+function setValue(selector,value,{parent = document} = {}){
+    parent.querySelector(`[data-${selector}]`).textContent = value
+}
+
+function getIconUrl(iconCode){
+    return `icons/${ICON_MAP.get(iconCode)}.svg`
+}
+const currentIcon = document.querySelector("[data-current-icon]")
+function renderCurrentWeather(current){
+    currentIcon.src = getIconUrl(current.iconCode)
+    setValue('current-temp',current.currentTemp)
+    setValue('current-high',current.highTemp)
+    setValue('current-low',current.lowTemp)
+    setValue('current-fl-high',current.highFeelsLike)
+    setValue('current-fl-low',current.lowFeelsLike)
+    setValue('current-wind',current.windSpeed)
+    setValue('current-precip',current.precip)
+
+
+    //It takes and render the current weather
+    
+
+}
--- weather.js
+++ weather.js
@@ -4,6 +4,9 @@ import axios from "axios";
 
 //https://api.open-meteo.com/v1/forecast?current=temperature_2m,apparent_temperature,precipitation,rain,showers,weather_code,wind_speed_10m&minutely_15=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m,temperature_80m,temperature_120m,temperature_180m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,daylight_duration,precipitation_sum,precipitation_hours,wind_speed_10m_max&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime
 //
+
+
+
 export function getWeather(lat, lon, timezone) {
   return axios
     .get(
@@ -19,8 +22,8 @@ export function getWeather(lat, lon, timezone) {
     .then(({ data }) => {
       return {
         current: parseCurrentWeather(data),
-        // daily: parseDailyWeather(data),
-        // hourly: parseHourlyWeather(data),
+        daily: parseDailyWeather(data),
+         hourly: parseHourlyWeather(data),
       }
     })
 }
@@ -39,7 +42,7 @@ function parseCurrentWeather({ current_weather, daily }) {
     apparent_temperature_min: [minFeelsLike],
     precipitation_sum: [precip],
   } = daily
-
+  
   return {
     currentTemp: Math.round(currentTemp),
     highTemp: Math.round(maxTemp),
@@ -50,4 +53,28 @@ function parseCurrentWeather({ current_weather, daily }) {
     precip: Math.round(precip * 100) / 100,
     iconCode,
   }
+}
+
+function parseDailyWeather({daily}){
+  return daily.time.map((time,index)=>{
+    return {
+      timestamp: time * 1000,
+      iconCode: daily.weathercode[index],
+      maxTemp:Math.round(daily.temperature_2m_max[index])
+
+    }
+  })
+}
+
+function parseHourlyWeather({hourly,current_weather}){
+     return hourly.time.map((time,index)=>{
+      return {
+        timestamp:time * 1000,
+        iconCode: hourly.weathercode[index],
+        temp:Math.round(hourly.temperature_2m[index]),
+        feelsLike:Math.round(hourly.apparent_temperature[index]),
+        windSpeed:Math.round(hourly.windspeed_10m[index]),
+        precip:Math.round(hourly.precipitation[index] * 100) / 100,
+      }
+     }).filter(({timestamp})=> timestamp >= current_weather.time * 1000)
 }
